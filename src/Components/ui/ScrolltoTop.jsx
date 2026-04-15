60d8d9a5c679549163261ef0b0176197cf3e2a24
juliosnchz264 <julio264edu@gmail.com>
2026-04-15 19:46:54 +0200

    fix case sensitive component

--- src/Components/ui/ScrolltoTop.jsx
+++ src/Components/ui/ScrolltoTop.jsx
@@ -1,6 +1,6 @@
 // src/Components/ui/ScrollToTop.jsx
 import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router';
 
 const ScrollToTop = () => {
     const { pathname } = useLocation();
