79175157fee56590823e56f10ac363a03789ebd2
juliosnchz264 <julio264edu@gmail.com>
2026-04-15 19:34:21 +0200

    fix: explicit case-sensitive paths for deployment

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router'
 import Navbar from '../Components/Navbar/Navbar'
 import Footer from '../Components/Footer/Footer'
-import ScrollToTop from '../Components/ui/ScrollToTop';
+import ScrollToTop from '../Components/ui/ScrolltoTop';
 import { ThemeProvider } from '../Context/ThemeContext'
 import SearchBar from '../Components/SearchBar/SearchBar'
 
